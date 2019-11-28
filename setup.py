fc44ee099bfbf95e85d8c5d0caf59ca5455eb15a
Hainguyen1210 <hainguyen.hainguyen1210@gmail.com>
2019-11-28 17:53:08 +0700

    try packaing

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='test',
+    version='0.0.1',
+    description='Parser for GML file',
+    url='git@github.com:Hainguyen1210/madballs.git',
+    author='hainguyen',
+    author_email='hainguyen.hainguyen1210@gmail.com',
+    license='unlicense',
+    packages=['test-packaging'],
+    zip_safe=False
+)
+
---
+++ test-packaging/__init__.py
@@ -0,0 +1,2 @@
+def haha():
+    print("it works") 
